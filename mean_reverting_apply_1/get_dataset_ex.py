970bfa79858a5e8a117c2e5f2d53336a6374540e
Allen <zzhan785@uwo.ca>
2020-08-14 08:51:59 +0000

    update

--- mean_reverting_apply_1/get_dataset_ex.py
+++ mean_reverting_apply_1/get_dataset_ex.py
@@ -1,12 +1,12 @@
 import dataset_handle as dh  
 from multiprocessing import Process
 dol_bar_file = "dol_bar.csv"
-p1 = Process(target=dh.store_raw_data,args=("XRPUSDT",'06 May 2018',"./XRPUSDT",dol_bar_file,"1m",))
+p1 = Process(target=dh.store_raw_data,args=("IOTAUSDT",'01 JUN 2018',"./IOTAUSDT",dol_bar_file,"1m",))
 p1.start() 
-p2 = Process(target=dh.store_raw_data,args=("NEOUSDT",'10 Nov 2017',"./NEOUSDT",dol_bar_file,"5min",))
+p2 = Process(target=dh.store_raw_data,args=("ETHUSDT",'01 AUG 2017',"./ETHUSDT",dol_bar_file,"5m",))
 p2.start()
-p3 = Process(target=dh.store_raw_data,args=("LTCUSDT",'10 Dec 2017',"./LTCUSDT",dol_bar_file,"5min",))
+p3 = Process(target=dh.store_raw_data,args=("ETCUSDT",'01 JUN 2018',"./ETCUSDT",dol_bar_file,"1m",))
 p3.start()
-p4 = Process(target=dh.store_raw_data,args=("BNBUSDT",'05 Nov 2017',"./BNBUSDT",dol_bar_file,"5min",))
+p4 = Process(target=dh.store_raw_data,args=("EOSUSDT",'01 MAY 2018',"./EOSUSDT",dol_bar_file,"1m",))
 p4.start()
 
