39b1b1ecafaea43b8d609fd42c30624754ac985d
Allen <zzhan785@uwo.ca>
2020-08-14 09:26:52 +0000

    udapte

--- mean_reverting_apply_1/test_get_train.py
+++ mean_reverting_apply_1/test_get_train.py
@@ -5,38 +5,38 @@ inputfile = outfolder+"dol_bar.csv"
 from multiprocessing import Process
 def preprocessing(outfolder):
     #get dollar bar 
-    handle = gtd.get_dol_bar_30(outfolder)
+    gtd.get_dol_bar_30(outfolder)
 
     #get features from dollar bar 
-    gtd = gtd.add_features(outfolder)
+    gtd.add_features(outfolder)
 
     #get label file 
-    labels = gtd.get_labels(outfolder)
+    gtd.get_labels(outfolder)
 
     # generate training data set
     gtd.get_training_dataset(outfolder)
 def preprocessing_15(outfolder):
     #get dollar bar 
-    handle = gtd.get_dol_bar_15(outfolder)
+    gtd.get_dol_bar_15(outfolder)
 
     #get features from dollar bar 
-    gtd = gtd.add_features(outfolder)
+    gtd.add_features(outfolder)
 
     #get label file 
-    labels = gtd.get_labels(outfolder)
+    gtd.get_labels(outfolder)
 
     # generate training data set
     gtd.get_training_dataset(outfolder)
-outfolder = ["EOSUSDT","ETHUSDT","IOTAUSDT","LTCUSDT","XRPUSDT"]
+outfolder = ["./EOSUSDT/","./ETHUSDT/","./IOTAUSDT/","./NEOUSDT/","./XRPUSDT/"]
 
 p1 = Process(target=preprocessing_15,args=(outfolder[0],))
 p1.start() 
-p1 = Process(target=preprocessing,args=(outfolder[1],))
+p2 = Process(target=preprocessing,args=(outfolder[1],))
 p2.start()
-p1 = Process(target=preprocessing,args=(outfolder[2],))
+p3 = Process(target=preprocessing,args=(outfolder[2],))
 p3.start()
-p1 = Process(target=preprocessing_15,args=(outfolder[3],))
+p4 = Process(target=preprocessing_15,args=(outfolder[3],))
 p4.start()
-p1 = Process(target=preprocessing_15,args=(outfolder[4],))
-p4.start()
+p5 = Process(target=preprocessing_15,args=(outfolder[4],))
+p5.start()
 
