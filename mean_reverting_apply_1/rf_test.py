0fcb8f08ec458a59dc4a14faf35a2ef5583c73b1
Allen <zzhan785@uwo.ca>
2020-08-14 11:03:32 +0000

    store model

--- mean_reverting_apply_1/rf_test.py
+++ mean_reverting_apply_1/rf_test.py
@@ -39,13 +39,13 @@ def training(folder,inputfile="x_y.csv"):
 
 outfolder = ["./EOSUSDT/","./ETHUSDT/","./IOTAUSDT/","./NEOUSDT/","./XRPUSDT/"]
 
-p1 = Process(target=training,args=(outfolder[0],))
-p1.start() 
-p2 = Process(target=training,args=(outfolder[1],))
-p2.start()
-# p3 = Process(target=training,args=(outfolder[2],))
-# p3.start()
-# p4 = Process(target=training,args=(outfolder[3],))
-# p4.start()
-# p5 = Process(target=training,args=(outfolder[4],))
-# p5.start()
+#p1 = Process(target=training,args=(outfolder[0],))
+#p1.start() 
+#p2 = Process(target=training,args=(outfolder[1],))
+#p2.start()
+p3 = Process(target=training,args=(outfolder[2],))
+p3.start()
+p4 = Process(target=training,args=(outfolder[3],))
+p4.start()
+p5 = Process(target=training,args=(outfolder[4],))
+p5.start()
