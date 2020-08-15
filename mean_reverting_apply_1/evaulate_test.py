30f9fdb806c8e2807bd22fc30310a81066f79b58
Zeyu Zhang <zzhan785@uwo.ca>
2020-08-15 00:36:49 -0400

    updast

--- mean_reverting_apply_1/evaulate_test.py
+++ mean_reverting_apply_1/evaulate_test.py
@@ -1,21 +1,25 @@
 from evaulate import evaulate
 import pandas as pd 
+from multiprocessing import Process
+def evaulate_ex(folder):
 
-def evaulate_ex(folder,subfolder):
-    subfolder = folder+subfolder
+    pre_file = folder+"test_predictions.csv"
+    label_file = folder+"labels.csv"
 
-    pre_file = subfolder+"test_predictions.csv"
-    label_file = folder+"labels_fix_close.csv"
-
-    ev = evaulate(label_file,pre_file,subfolder)
+    ev = evaulate(label_file,pre_file,folder)
     ev.long_stats()
     ev.short_stats()
 
-folder = "./BTCUSDT/"
-subfolder= ["min-fix5/","min-fix-reg/","min-fix2/","min-fix/","min-fix3/"]
-# subfolder= []
-# for i in range(1,5):
-#     subfolder.append('close-fix'+str(i)+"/")
-for i in subfolder:
-    evaulate_ex(folder,i)
+outfolder = ["./EOSUSDT/","./ETHUSDT/","./IOTAUSDT/","./NEOUSDT/","./XRPUSDT/"]
+
+p1 = Process(target=evaulate_ex,args=(outfolder[0],))
+p1.start() 
+p2 = Process(target=evaulate_ex,args=(outfolder[1],))
+p2.start()
+# p3 = Process(target=evaulate_ex,args=(outfolder[2],))
+# p3.start()
+# p4 = Process(target=evaulate_ex,args=(outfolder[3],))
+# p4.start()
+# p5 = Process(target=evaulate_ex,args=(outfolder[4],))
+# p5.start()
 
