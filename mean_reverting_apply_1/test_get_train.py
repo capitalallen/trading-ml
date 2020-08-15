d0ca2719478b2f91acdf67539c42145b4e161da2
Zeyu Zhang <zzhan785@uwo.ca>
2020-08-14 22:32:53 -0400

    update

--- mean_reverting_apply_1/test_get_train.py
+++ mean_reverting_apply_1/test_get_train.py
@@ -11,21 +11,32 @@ def preprocessing(outfolder):
     gtd = gtd.add_features(outfolder)
 
     #get label file 
-    labels = gtd.get_labels_no_side(outfolder)
+    labels = gtd.get_labels(outfolder)
 
     # generate training data set
     gtd.get_training_dataset(outfolder)
+def preprocessing_15(outfolder):
+    #get dollar bar 
+    handle = gtd.get_dol_bar_15(outfolder)
 
+    #get features from dollar bar 
+    gtd = gtd.add_features(outfolder)
+
+    #get label file 
+    labels = gtd.get_labels(outfolder)
+
+    # generate training data set
+    gtd.get_training_dataset(outfolder)
 outfolder = ["EOSUSDT","ETHUSDT","IOTAUSDT","LTCUSDT","XRPUSDT"]
 
-p1 = Process(target=preprocessing,args=(outfolder[0],))
+p1 = Process(target=preprocessing_15,args=(outfolder[0],))
 p1.start() 
 p1 = Process(target=preprocessing,args=(outfolder[1],))
 p2.start()
 p1 = Process(target=preprocessing,args=(outfolder[2],))
 p3.start()
-p1 = Process(target=preprocessing,args=(outfolder[3],))
+p1 = Process(target=preprocessing_15,args=(outfolder[3],))
 p4.start()
-p1 = Process(target=preprocessing,args=(outfolder[4],))
+p1 = Process(target=preprocessing_15,args=(outfolder[4],))
 p4.start()
 
