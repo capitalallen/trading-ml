960c5ce19560f556ea93291320165eb4f7ff53d0
Zeyu Zhang <zzhan785@uwo.ca>
2020-08-18 12:23:13 -0400

    w

--- live_trading/BTCUSDT/trading_ex.py
+++ live_trading/BTCUSDT/trading_ex.py
@@ -1,5 +1,6 @@
 import trading 
 
-db_name "../pair_db/features.sqlite"
+db_name= "../pair_db/features.sqlite"
 record_name="BTCUSDT"
-t = trading
+t = trading.Trading(pair="BTCUSDT",db_name=db_name,record_name=record_name,model_path="model.joblib",columns_path="column_order.json")
+t.live_trading()
