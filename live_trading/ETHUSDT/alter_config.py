5eda9b6a3e92d8764c61e47c2cadd1f93820ca35
Allen <zzhan785@uwo.ca>
2020-08-20 15:09:39 +0000

    w

--- live_trading/ETHUSDT/alter_config.py
+++ live_trading/ETHUSDT/alter_config.py
@@ -15,9 +15,14 @@ def get_strategy(pair="ETHUSDT"):
     cc = change_config.Change_config()
     print(cc.get_pair_config(pair))
 
+# lev_long lev_short
+def change_lev(pair,lev_type,rate):
+    cc = change_config.Change_config()
+    cc.update_config(pair,lev_type,rate)
 pair = "ETHUSDT"
 #change_strategy(pred=1,side=-1,pair='ETHUSDT',strategy="long_strategy")
 #change_strategy(pred=0,side=-1,pair='ETHUSDT',strategy="short_strategy")
 change_discount(pair="ETHUSDT",type="long_p",rate=0.005)
+change_lev(pair,'lev_long',40)
 #change_discount(pair="ETHUSDT",type="short_p",rate=0)
 get_strategy(pair)
