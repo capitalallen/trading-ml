494b7de48a8705754bd49d1dfe1b1021bd0df0a1
Zeyu Zhang <zzhan785@uwo.ca>
2020-08-20 11:17:58 -0400

    e

--- live_trading/price_getter/price.py
+++ live_trading/price_getter/price.py
@@ -9,5 +9,5 @@ output price
 def get_price(pair):
     url = "https://api.binance.com/api/v3/ticker/price?symbol="+pair 
     headers = {"Content-Type": "application/json"}
-    return  round(float(ast.literal_eval(requests.get(url, headers=headers).content.decode("UTF-8"))['price']),3)
+    return  round(float(ast.literal_eval(requests.get(url, headers=headers).content.decode("UTF-8"))['price']),2)
 
