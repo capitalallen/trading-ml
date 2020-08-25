2d92cf890d5e01c65789d69d2d352fed1987eeee
Allen <zzhan785@uwo.ca>
2020-08-25 14:42:08 +0000

    finished

--- live_trading/trade/temp_trading.py
+++ live_trading/trade/temp_trading.py
@@ -1,9 +1,9 @@
 import trailing 
 from multiprocessing import Process
 
-#t1 = trailing.Trailing(29.99, 'short',1,1, 2, "ETHUSDT",391.69)
-#Process(target=t1.trailing_stop_short).start()
-t2 = trailing.Trailing(0.965, 'long',1,1, 2, "BTCUSDT",11702.39)
+t1 = trailing.Trailing(31.63, 'long',2,1, 2, "ETHUSDT",385.76)
+Process(target=t1.trailing_stop_short).start()
+t2 = trailing.Trailing(15, 'short',1,1, 2, "ETHUSDT",384.33)
 Process(target=t2.trailing_stop_long).start()
 #t3 = trailing.Trailing(101, 'long',1,1, 2, "BNBUSDT",21.996)
 #Process(target=t3.trailing_stop_long).start()
