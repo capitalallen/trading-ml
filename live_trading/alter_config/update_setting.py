a8c28695441a3e34aaafe6f9fb0756ee5ac3069e
Zeyu Zhang <zzhan785@uwo.ca>
2020-08-27 23:14:24 -0400

    w

--- live_trading/alter_config/update_setting.py
+++ live_trading/alter_config/update_setting.py
@@ -5,10 +5,10 @@ def update_settings(cols):
     for i in cols: 
         cc.add_amount_entry(i)
 
-# cols = ["BNBUSDT","BTCUSDT","ETHUSDT","XRPUSDT"]
-# update_settings(cols)
-cc = change_config.Change_config()
-# cc.update_long_trade_avl("BTCUSDT",1)
-cc.update_short_trade_avl("BTCUSDT",-1)
-# cc.get_trade_avl('BTCUSDT','long')
-cc.get_trade_avl('BTCUSDT','short')
+cols = ["BNBUSDT","BTCUSDT","ETHUSDT","XRPUSDT"]
+update_settings(cols)
+# cc = change_config.Change_config()
+# # cc.update_long_trade_avl("BTCUSDT",1)
+# cc.update_short_trade_avl("BTCUSDT",-1)
+# # cc.get_trade_avl('BTCUSDT','long')
+# cc.get_trade_avl('BTCUSDT','short')
