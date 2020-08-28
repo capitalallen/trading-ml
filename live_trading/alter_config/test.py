ee7877c7ac0f25bd5eaef6cd49fef1875ce5514b
Allen <zzhan785@uwo.ca>
2020-08-28 23:14:20 +0000

    finished

--- live_trading/alter_config/test.py
+++ live_trading/alter_config/test.py
@@ -1,16 +1,16 @@
 import pymongo
 import change_config
-#cc=change_config.Change_config() 
-#cc.update_trading_num("long",3)
+cc=change_config.Change_config() 
+#cc.update_trading_num("long",-2)
 #cc.update_trading_num("short",2)
-#print(cc.get_trading_num())
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["configs"]
-mycol = mydb["total_trade_num"]
-mycol.update({"_id":mycol.find_one()['_id']},{"$set":{"transaction_long":100,"transaction_short":150}})
+print(cc.get_trading_num())
+#myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+#mydb = myclient["configs"]
+#mycol = mydb["total_trade_num"]
+#mycol.update({"_id":mycol.find_one()['_id']},{"$set":{"transaction_long":100,"transaction_short":150}})
 #print(mycol.find_one())
-# mydict = {"long":5,"short":5,"time":None}
-# mycol.insert_one(mydict)
+#mydict = {"long":10,"short":10,"time":None}
+#mycol.insert_one(mydict)
 
 #cc = change_config.Change_config()
 # # print(type(cc.query_config("ETHUSDT","long")))
