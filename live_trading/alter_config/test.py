ad88a8072e870670b2db90b71b6799dcbe402cd1
Allen <zzhan785@uwo.ca>
2020-09-05 21:03:09 +0000

    finished

--- live_trading/alter_config/test.py
+++ live_trading/alter_config/test.py
@@ -1,20 +1,20 @@
 import pymongo
 import change_config
-#cc=change_config.Change_config() 
+cc=change_config.Change_config() 
 #cc.update_trading_num("long",5)
-#cc.update_trading_num("short",2)
-#print(cc.get_trading_num())
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["configs"]
-mycol = mydb["total_trade_num"]
+#cc.update_trading_num("short",3)
+print(cc.get_trading_num())
+#myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+#mydb = myclient["configs"]
+#mycol = mydb["total_trade_num"]
 #mycol.update({"_id":mycol.find_one()['_id']},{"$set":{"transaction_long":100,"transaction_short":150}})
-print(mycol.find_one())
+#print(mycol.find_one())
 #mydict = {"long":10,"short":10,"time":None}
 #mycol.insert_one(mydict)
 
 #cc = change_config.Change_config()
 # # print(type(cc.query_config("ETHUSDT","long")))
-#cc.update_trading_num("long",1)
+#cc.update_trading_num("long",5)
 #cc.update_trading_num("long",1)
 #cc.init_pair_config("XRPUSDT")
 # cc.init_pair_config("BNBUSDT")
